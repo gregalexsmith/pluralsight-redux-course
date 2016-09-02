9ff69f76d71e55319abdbb843255f0d156d0caac
gregalexsmith <gregalexsmith@gmail.com>
2016-09-02 11:58:12 -0400

    react running

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Aboutpage extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>This app uses React, Redux and React Router in ES6 for ultra responsive web apps.</p>
+      </div>
+    );
+  }
+}
+
+export default Aboutpage;
