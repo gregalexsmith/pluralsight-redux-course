ce31e12d5d0f022f5d7827feb24b76c54a5f018d
gregalexsmith <gregalexsmith@gmail.com>
2016-09-03 14:15:14 -0400

    mapdispatchtoprops

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -21,7 +21,7 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave() {
-    this.props.dispatch(courseActions.createCourse(this.state.course));
+    this.props.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -49,8 +49,8 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  courses: PropTypes.array.isRequired
+  courses: PropTypes.array.isRequired,
+  createCourse: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -60,12 +60,16 @@ function mapStateToProps(state, ownProps) {
   };
 }
 
-function mapDispatchToProps() {
-
+function mapDispatchToProps(dispatch) {
+  return {
+    createCourse: (course) => {
+      dispatch(courseActions.createCourse(course));
+    }
+  };
 }
 
 
-export default connect(mapStateToProps)(CoursesPage);
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
 // this ^^ is equivalent to this vv ...connect returns a funciton that calls another function with CoursePages parameter
 // const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps);
 // export default connectedStateAndProps(CoursesPage);
