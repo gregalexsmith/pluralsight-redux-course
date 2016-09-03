64a7e7080750ef39673de3a9826a5660bedaa3cc
gregalexsmith <gregalexsmith@gmail.com>
2016-09-03 14:43:18 -0400

    moving into async

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,13 +1,12 @@
+import * as types from '../actions/actionTypes';
+
+
 export default function courseReducer(state = [], action) {
   switch(action.type) {
-    case 'CREATE_COURSE':
-    // cant do this....
-      //  state.push(action.course);
-      //  return state;
-    //instead....
-    debugger;
-    return [...state,      // ... es6 spread: explodes the array
-                          // like taking the array and defining all of the values inline
+    case types.CREATE_COURSE:
+
+    return [
+      ...state,
       Object.assign({}, action.course)
     ];
 
